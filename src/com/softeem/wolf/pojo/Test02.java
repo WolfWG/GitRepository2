a5ecb692426857aa2180283a5a755ad1d5a92c2c
wolf <3342968446@qq.com>
2021-10-29 23:27:23 +0800

    添加Test02.java文件

---
+++ src/com/softeem/wolf/pojo/Test02.java
@@ -0,0 +1,11 @@
+package com.softeem.wolf.pojo;
+
+/**
+ * Created by 苍狼
+ * Time on 2021-10-29
+ */
+public class Test02 {
+    public static void main(String[] args) {
+        System.out.println("我是Test02的测试类.....");
+    }
+}
