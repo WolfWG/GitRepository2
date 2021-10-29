97480f05d4f63e5466bec2ff90adb40c0d135325
wolf <3342968446@qq.com>
2021-10-29 23:20:34 +0800

    添加Test.java文件

---
+++ src/com/softeem/wolf/pojo/Test.java
@@ -0,0 +1,11 @@
+package com.softeem.wolf.pojo;
+
+/**
+ * Created by 苍狼
+ * Time on 2021-10-29
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("我是来测试的....");
+    }
+}
